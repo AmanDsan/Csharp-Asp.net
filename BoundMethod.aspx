d5c2ac7bae94f24451901107d8498056aba68103
AmanDsan <126654741+AmanDsan@users.noreply.github.com>
2024-06-06 18:15:19 +0530

    Add files via upload

---
+++ BoundMethod.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="BoundMethod.aspx.cs" Inherits="BoundMethod" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .sty_yokai {
+    background-color:lightskyblue;
+    color:black;
+}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+    <table>
+    <tr>
+        <td>
+            <asp:Label ID="Label1" runat="server" Text="ID"></asp:Label>
+        </td>
+        <td>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+        </td>
+        <td>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Label ID="Label3" runat="server" Text="City"></asp:Label>
+        </td>
+        <td>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Value="New Delhi"></asp:ListItem>
+                <asp:ListItem Value="Bangalore"></asp:ListItem>
+                <asp:ListItem Value="Nagpur"></asp:ListItem>
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <asp:Label ID="Label4" runat="server" Text="Salary"></asp:Label>
+        </td>
+        <td>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+</table>
+
+        
+<asp:Button ID="Button1" runat="server" Text="Submit"  OnClick="ShowData" Width="113px"/>
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" AllowSorting="True" AllowPaging="true" PageSize="15" 
+            OnRowDataBound="GridView1_RowDataBound"
+            OnPageIndexChanging="Gridpaging"
+            OnSorting="GridView1_Sorting"
+             OnRowCreated="GridView1_RowCreated"
+            OnDataBound="GridView1_DataBound" >
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="Id"  SortExpression="id"/>
+                <asp:BoundField DataField="Name" HeaderText="Name"  SortExpression="name"/>
+                <asp:BoundField DataField="City" HeaderText="City"  SortExpression="city"/>
+                <asp:BoundField DataField="Salary" HeaderText="Salary"  SortExpression="salary"/>
+            </Columns>
+        </asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ BoundMethod.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BoundMethod : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+
+    protected void ShowData(object sender, EventArgs e)
+    {
+
+        SqlConnection connec = new SqlConnection(ConfigurationSettings.AppSettings["ConnectionString"].ToString());
+        connec.Open();
+        SqlCommand cmd = new SqlCommand("INSERT INTO GridBound_table ([ID],[Name], [City],[Salary]) VALUES (@Value1, @Value2, @Value3, @Value4)", connec);
+        cmd.Parameters.AddWithValue("@Value1", TextBox1.Text);
+        cmd.Parameters.AddWithValue("@Value2", TextBox2.Text);
+        cmd.Parameters.AddWithValue("@Value3", DropDownList1.SelectedItem.Text);
+        cmd.Parameters.AddWithValue("@Value4", TextBox4.Text);
+
+       
+        cmd.ExecuteNonQuery();
+        connec.Close();
+
+        GridDat();
+    }
+
+   /* protected void GridDat()
+    {
+        string query = "Select * From GridBound_table";
+        SqlConnection connec = new SqlConnection(ConfigurationSettings.AppSettings["ConnectionString"].ToString());
+        SqlCommand com = new SqlCommand(query, connec);
+        connec.Open();
+        SqlDataReader reader = com.ExecuteReader();
+        if (reader.HasRows)
+        {
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+        }
+        reader.Close();
+
+    }*/
+
+
+    protected void GridDat()
+    {
+        string query = "Select * From GridBound_table";
+        SqlConnection connec = new SqlConnection(ConfigurationSettings.AppSettings["ConnectionString"].ToString());
+        SqlCommand com = new SqlCommand(query, connec);
+        SqlDataAdapter adapter = new SqlDataAdapter(com);
+        DataTable dt = new DataTable();
+        adapter.Fill(dt);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+        ViewState["dirState"] = dt;
+        ViewState["sortdr"] = "Asc";
+    }
+
+    protected void GridView1_Sorting(object sender, GridViewSortEventArgs e)
+    {
+        DataTable sortdat = (DataTable)ViewState["dirState"];
+        if (sortdat.Rows.Count > 0)
+        {
+            if (Convert.ToString(ViewState["sortdr"]) == "Asc")
+            {
+                sortdat.DefaultView.Sort = e.SortExpression + " Desc";
+                ViewState["sortdr"] = "Desc";
+            }
+            else
+            {
+                sortdat.DefaultView.Sort = e.SortExpression + " Asc";
+                ViewState["sortdr"] = "Asc";
+            }
+            GridView1.DataSource = sortdat;
+            GridView1.DataBind();
+        }
+
+    }
+
+    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    { 
+       
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+           
+            if (Convert.ToInt32(e.Row.Cells[3].Text) < 1000)
+            {
+                e.Row.BackColor = Color.LavenderBlush;
+            }
+        }
+    }
+
+    protected void Gridpaging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        GridView1.DataSource = ViewState["Paging"];
+        GridView1.DataBind();
+
+        GridDat();
+    }
+
+    protected void GridView1_RowCreated(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            e.Row.CssClass = "sty_yokai";
+        }
+    }
+
+    protected void GridView1_DataBound(object sender, EventArgs e)
+    {
+        
+    }
+
+}
